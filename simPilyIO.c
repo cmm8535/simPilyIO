6b30b0d0cc328173684b54e1ee973caaa23d618a
Cameron Myron <cmm8535@g.rit.edu>
2020-05-07 02:32:54 +0000

    Random commit. Added the "addPin" function and implemented direction to the pins. Think I'm going to redo most of the code in what will probably be a more effecient way (at very least it will keep pin data in memory opposed to in what I think is storage, which should speed up looking up pin data... Also it will allow the program to easily ignore commands that have no effect (like setting a pin value to 1 even if it is already 1)).

--- simPilyIO.c
+++ simPilyIO.c
@@ -6,16 +6,56 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #define GPIO_PATH "/sys/class/gpio"
 #define PATH_BUFFER 100
 
+///Initalizes the given pin for use
+int addPin(short pin, char* direction){
+  char path[PATH_BUFFER];
+  snprintf(path, PATH_BUFFER, "%s/export", GPIO_PATH);
+  FILE* f;
+  if(!(f=fopen(path, "w"))){
+    fprintf(stderr, "Couldn't Open %s\n", path);
+    return 1;
+  }
+  fprintf(f, "%hi", pin);
+  fclose(f);
+  snprintf(path, PATH_BUFFER, "%s/gpio%hi/direction", GPIO_PATH, pin);
+  if(!(f=fopen(path, "w"))){
+    fprintf(stderr, "Couldn't Open %s\n", path);
+    return 1;
+  }
+  fprintf(f, "%s", direction);
+  fclose(f);
+  return 0;
+}
+
+///Un-initalizes the given pin
+int removePin(short pin){
+  (void)pin;
+  return 0;
+}
+
 ///Sets the given gpio pin to the given value
 void setPin(short pin, short value){
   char path[PATH_BUFFER];
-  sprintf(path, "%s/gpio%hi/value", GPIO_PATH, pin);
   FILE* f;
+  snprintf(path, PATH_BUFFER, "%s/gpio%hi/direction", GPIO_PATH, pin);
+  if(!(f=fopen(path, "r"))){
+    fprintf(stderr, "Couldn't Open %s\n", path);
+    return;
+  }
+  char dir[3];
+  fscanf(f, "%3s", dir);
+  fclose(f);
+  if(strncmp(dir, "out", 3)){
+    fprintf(stderr, "This Is An Input Pin\n");
+    return;
+  }
+  snprintf(path, PATH_BUFFER, "%s/gpio%hi/value", GPIO_PATH, pin);
   if(!(f=fopen(path, "w"))){
     fprintf(stderr, "Couldn't Open %s\n", path);
     return;
@@ -27,7 +67,7 @@ void setPin(short pin, short value){
 ///Gets the value of the given gpio pin
 short getPin(short pin){
   char path[PATH_BUFFER];
-  sprintf(path, "%s/gpio%hi/value", GPIO_PATH, pin);
+  snprintf(path, PATH_BUFFER, "%s/gpio%hi/value", GPIO_PATH, pin);
   FILE* f;
   if(!(f=fopen(path, "r"))){
     fprintf(stderr, "Couldn't Open %s\n", path);
--- simPilyIO.h
+++ simPilyIO.h
@@ -11,12 +11,50 @@
  *@Email: cmm8535@g.rit.edu
  */
 
+///This part is for an idea to speed up functions to only access files
+///When neccessary (like setting a value that it doesn't have already)
+struct pin{
+  short num:5;
+  short val:1;
+  short dir:1;
+};
+
+typedef struct pin* pin_t;
+
+/**
+ *Initializes the given pin for use (set to 0 by default)
+ *
+ *ERRORS:
+ *  -Invalid GPIO number
+ *  -Direction is invalid
+ *  -Program doesn't have root priveledges (can't access a needed path)
+ *
+ *@Param pin: GPIO number
+ *@Param direction: String indicating the pin's direction ("in" or "out")
+ *@Return: 0 if successful, 1 otherwise
+ */
+int addPin(short pin, char* direction);
+
+/**
+ *Un-initializes the given pin when not needed anymore
+ *
+ *ERRORS:
+ *  -If the given pin is uninitialized (or invalid)
+ *  -Program doesn't have root priveledges (can't access a needed path)
+ *
+ *@Param pin: GPIO number
+ *@Return: 0 if successful, 1 otherwise
+ */
+int removePin(short pin);
+
 /**
  *Sets the value of a given gpio pin to the given value
  *
  *ERRORS:
  *  -If a value that is not a 1 or 0 is given it will be treated as a 1
  *  -If the given pin is uninitialized (or invalid), nothing happens
+ *  -The given pin has a direction of "in"
+ *  -Program doesn't have root priveledges (can't access a needed path)
  *
  *@Param pin: GPIO number
  *@Param value: 1 to set the pin "high", 0 to set the pin "low"
@@ -30,6 +68,7 @@ void setPin(short pin, short value);
  *  -If the given pin is uninitialized (or invalid), -1 is returned
  *  -If the pin somehow doesn't have a value (which I think is impossible, but
  *  need this part to suppress errors), -1 is returned
+ *  -Program doesn't have root privelegdes (can't access a needed path)
  *
  *@Param pin: GPIO number
  *@Return: Value of the given gpio pin (1 if "high", 0 if "low")
